61558e044683983dcbafa77f120e9b569cb55c3a
Andrew Cheng <andrewbcheng@gmail.com>
2024-11-25 13:13:25 -0800

    make past bills into docs embed

--- src/pages/Office/PastBills.page.tsx
+++ src/pages/Office/PastBills.page.tsx
@@ -1,21 +1,22 @@
 function PastBills() {
   return (
-      <>
-        <div className="min-h-[90vh] mx-20 mt-48 flex flex-col gap-10 [&>*:last-child]:mb-40">
-          <div className="w-1/2 flex flex-center">
-            <div>
-                <h2>Bills we are working on</h2>
-                <div>carousel</div>
-                <div>links</div>
-            </div>
-            <div>
-                <h2>Bills we've passed</h2>
-                <div>carousel</div>
-                <div>links</div>
-            </div>
+    <>
+      <div className="min-h-[90vh] mx-20 mt-48 flex flex-col gap-10 [&>*:last-child]:mb-40">
+        <div className="w-ful text-center">
+          <header className="w-ful text-center">
+            <h1 className="text-red-50 my-8">Our Bills</h1>
+          </header>
+          <div className="w-full flex justify-center">
+            <iframe
+              src="https://docs.google.com/document/d/e/2PACX-1vTkWNBZP_-2L7bRIl57PBJF0KnAXfkHVNkDcN6QrshUYrt1x05OX4tFeoT2NEVb6-cCSDMycmlkEHM4/pub?embedded=true"
+              width="60%"
+              height="900px"
+              title="Past Bills"
+            />
           </div>
-        </div>  
-      </>
+        </div>
+      </div>
+    </>
   );
 }
 
