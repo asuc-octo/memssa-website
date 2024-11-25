c45267aec33b94c4cf29a7521757f8aaf54b902a
Timothy Zheng <tizheng3@berkeley.edu>
2024-11-24 22:35:59 -0800

    basic teams

--- src/pages/Office/Team.page.tsx
+++ src/pages/Office/Team.page.tsx
@@ -1,22 +1,27 @@
+import AboutUsImage from '../../assets/images/AboutUsImage.jpg'
+import TeamHs from '../../assets/images/teampageheadshot.jpeg'
+import ChiefsTeam from '../../assets/images/chiefsimg.jpg'
+
+
 function Team() {
   return (
+        // header here
+        // page div
     <>
       <div className="min-h-screen mx-20 mt-24 flex flex-col gap-12">
-        {/* image */}
-        <div className="flex flex-col items-center gap-16">
-          <div className="w-full h-80 bg-gray-200 flex items-center justify-center">
-          </div>
+        <div className="h-11/12 flex flex-col items-center gap-16 text-black">
+        <img src={AboutUsImage} alt="Team Image" className="h-auto justify-center" />
           <h1 className="text-4xl">Our Team</h1>
         </div>
 
         <div className="flex flex-col gap-16">
           {/* senator */}
           <div className="flex flex-col md:flex-row items-center justify-center gap-52">
-            <div className="w-80 h-80 bg-gray-200 flex items-center justify-center">
-              {/* image */}
+            <div className="w-1/3 h-1/3 flex items-center justify-center">
+            <img src={TeamHs} alt="Senator Medina Danish" className="h-auto" />
             </div>
-            <div className="flex flex-col text-center items-center self-start">
-              <h1 className="text-2xl font-bold">
+            <div className="flex flex-col text-center items-center self-start text-black justify-center">
+              <h1 className="text-2xl">
                 Meet MEMSSA<br/>Senator<br/>Medina Danish
               </h1>
               <p className="mt-2">about.</p>
@@ -25,24 +30,22 @@ function Team() {
 
           {/* chiefs of staff */}
           <div className="flex flex-col md:flex-row-reverse items-center justify-center gap-52">
-            <div className="w-80 h-80 bg-gray-200 flex justify-center">
-              {/* image */}
+            <div className="w-1/3 h-1/3 flex justify-center">
+            <img src={ChiefsTeam} alt="Chiefs Image" className="h-auto" />
             </div>
-            <div className="flex flex-col text-center self-start">
+            <div className="flex flex-col text-center self-start text-black" >
               <h1 className="text-2xl">
                 Meet Our<br/>Chiefs of Staff
               </h1>
-              <p className="mt-2">about</p>
+              <p className="mt-2 text-black">about</p>
             </div>
           </div>
         </div>
         {/* departments */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 fonr text-black">
           <h1 className="text-center">OUR DEPARTMENTS!</h1>
           <h2 className="text-center">Department Name</h2>
-          <div className="w-full h-64 bg-gray-300 flex items-center justify-center">
-            {/* uhh idk how to do the arrows */}
-          </div>
+          <img src={AboutUsImage} alt="Team Image" className="h-auto" />
         </div>
       </div>
     </>
