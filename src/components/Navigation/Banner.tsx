79e1e935124e9336eeba7b3f9e2f5db9ad65055a
Richa Jos <richajos34@berkeley.edu>
2024-12-05 20:23:37 -0800

    rouded

--- src/components/Navigation/Banner.tsx
+++ src/components/Navigation/Banner.tsx
@@ -5,6 +5,8 @@ interface BannerProps {
 }
 
 const Banner: React.FC<BannerProps> = ({ title }) => {
+    const isRSOLeaderResources = title === "RSO Leader Resources";
+
     return (
         <>
             <div className='relative text-center'>
@@ -13,15 +15,19 @@ const Banner: React.FC<BannerProps> = ({ title }) => {
                     className="w-full"
                 />
                 {/* black shadow */}
-                <h1 className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/5 text-white text-9xl font-bold drop-shadow-[0_4px_4px_rgba(0,0,0,1)]">
+                <h1 className={`absolute ${isRSOLeaderResources ? 'top-1/4 text-8xl' : 'top-1/4 text-9xl'} left-1/2 transform -translate-x-1/2 -translate-y-1/5 text-white font-bold drop-shadow-[0_4px_4px_rgba(0,0,0,1)]`}>
                     {title}
                 </h1>
                 {/* red shadow */}
-                <h1 className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/5 text-white text-9xl font-bold drop-shadow-[0_4px_4px_rgba(178,103,94,1)]">
+                <h1 className={`absolute ${isRSOLeaderResources ? 'top-1/4 text-8xl' : 'top-1/4 text-9xl'} left-1/2 transform -translate-x-1/2 -translate-y-1/5 text-white font-bold drop-shadow-[0_4px_4px_rgba(178,103,94,1)]`}>
                     {title}
                 </h1>
                 {/* subtitle */}
-                {title == "MEMSSA" && (<h2 className="absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/5">middle east, muslim, sikh, south asian association</h2>)}
+                {title === "MEMSSA" && (
+                    <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/5">
+                        middle east, muslim, sikh, south asian association
+                    </h2>
+                )}
             </div>
         </>
     )
