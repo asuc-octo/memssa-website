cab75dacfd11cfebb52409c3992fe29802eda862
vivan <vivan.sinha.003@gmail.com>
2024-12-05 18:32:45 -0800

    home button styling

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,7 +1,20 @@
 import { FourToOneGrid } from '../components/components/Layout/Columns.layout';
 import { WidePageLayout } from '../components/components/Layout/Page.layout'
 import AboutUsImage from '../assets/images/AboutUsImage.jpg'
+import HomeButtonBackgroundImage from '../assets/images/homeButtonBackground.jpg'
 import Banner from '../components/Navigation/Banner'
+
+function BackgroundImage() {
+  return (
+    <div className="relative w-full h-full">
+      <img
+        src={HomeButtonBackgroundImage}
+        alt="Background"
+        className="absolute top-0 left-0 w-full h-full object-cover rounded-xl"
+      />
+    </div>
+  );
+}
 
 function Home() {
     return (
@@ -15,33 +28,48 @@ function Home() {
               <div className="w-1/2 px-8 flex flex-col items-center">
                 <h3>About Us</h3>
                 <p className="text-lg mb-4">
-                MEMSSA represents the University of California, Berkeley by advocating for an academic, administrative, and operates on grassroots levels. The team mission emphasizes the importance of mobilizing organizations that indicate their intent on serving and / or representing one or more of the following identities: Middle Eastern, Muslim, Sikh, or South Asian.
+                  MEMSSA represents the University of California, Berkeley by advocating for an academic, administrative, and operates on grassroots levels. The team mission emphasizes the importance of mobilizing organizations that indicate their intent on serving and / or representing one or more of the following identities: Middle Eastern, Muslim, Sikh, or South Asian.
                 </p>
-                  <button
+                <button
                   className="bg-dark-red text-light-brown py-2 px-8 rounded-xl hover:bg-light-purple"
                   onClick={() => window.location.href = '/office/pastbills'}
-                  >
+                >
                   Link to Constitution Bills
-                  </button>
-              </div>
+                </button>
+                </div>
               </section>
-            <FourToOneGrid>
-              <button className="bg-dark-red text-light-brown py-2 px-8 rounded-xl hover:bg-light-purple">
-                Join Our Office
-              </button>
-              <button className="bg-dark-red text-light-brown py-2 px-8 rounded-xl hover:bg-light-purple">
-                Join Our Mailing List
-              </button>
-              <button
-                className="bg-dark-red text-light-brown py-2 px-8 rounded-xl hover:bg-light-purple"
-                onClick={() => window.location.href = '/calendar'}
-              >
-                Calendar
-              </button>
-              <button className="bg-dark-red text-light-brown py-2 px-8 rounded-xl hover:bg-light-purple">
-                Anonymous Suggestions
-              </button>
-            </FourToOneGrid>
+              <FourToOneGrid>
+                <div className="relative w-full h-64">
+                  <BackgroundImage/>
+                  <button className="bg-light-brown text-dark-red py-2 px-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold w-3/4">
+                    Join Our Office
+                  </button>
+                </div>
+
+                <div className="relative w-full h-64">
+                  <BackgroundImage/>
+                  <button className="bg-light-brown text-dark-red py-2 px-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold w-3/4">
+                    Join Our Mailing List
+                  </button>
+                </div>
+
+                <div className="relative w-full h-64">
+                  <BackgroundImage/>
+                  <button
+                    className="bg-light-brown text-dark-red py-2 px-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold w-3/4"
+                    onClick={() => (window.location.href = '/calendar')}
+                  >
+                    Calendar
+                  </button>
+                </div>
+
+                <div className="relative w-full h-64">
+                  <BackgroundImage/>
+                  <button className="bg-light-brown text-dark-red py-2 px-2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold w-3/4">
+                    Anonymous Suggestions
+                  </button>
+                </div>
+              </FourToOneGrid>
           </WidePageLayout>
         </>
     );
